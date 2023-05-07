c2bff2a20072dca52ee8145ac2279a43b9e1ec4f
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-05-06 21:45:11 -0600

    Reimplemented the entire BrickboardTheme

--- app/src/main/java/com/example/brickboard/MainActivity.kt
+++ app/src/main/java/com/example/brickboard/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.brickboard
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.brickboard.ui.theme.BrickboardTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +19,7 @@ class MainActivity : ComponentActivity() {
             BrickboardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxWidth()
                 ) {
                     Greeting("Android")
                 }
@@ -34,4 +34,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun GreetingPreview() {
+    BrickboardTheme {
+        Greeting("Android")
+    }
 }
--- app/src/main/java/com/example/brickboard/ui/theme/Theme.kt
+++ app/src/main/java/com/example/brickboard/ui/theme/Theme.kt
@@ -1,43 +1,76 @@
 package com.example.brickboard.ui.theme
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.lightColors
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 
-private val lightColourPalette = lightColors(
+private fun lightPalette() = BrickboardColours(
     primary = brickBoardBlue,
+    primaryVariant = darkBrickBoarBlue,
     secondary = brickBoardRed,
     onPrimary = onBrickBoardBlue,
     onSecondary = onBrickBoardRed,
     surface = brickBoardSurface,
-    onSurface = brickBoardSurface,
+    onSurface = onBrickBoardSurface,
     background = brickBoardBackground,
-    onBackground = onBrickBoardBackground
+    onBackground = onBrickBoardBackground,
+    isLight =  true
+)
+
+private fun darkPalette() = BrickboardColours(
+    primary = brickBoardBlue,
+    primaryVariant = darkBrickBoarBlue,
+    secondary = brickBoardRed,
+    onPrimary = onBrickBoardBlue,
+    onSecondary = onBrickBoardRed,
+    surface = brickBoardDarkSurface,
+    onSurface = onBrickBoardDarkSurface,
+    background = brickBoardDarkBackground,
+    onBackground = onBrickBoardDarkBackground,
+    isLight =  false
 )
 
 val LocalReplacementTypography = staticCompositionLocalOf {
-    BrickboardTypography
+    BrickboardTypography()
 }
+
+val LocalReplacementColours = compositionLocalOf {
+    lightPalette()
+}
+
 
 @Composable
 fun BrickboardTheme(
+    typography: BrickboardTypography = BrickboardTheme.typography,
+    colours: BrickboardColours = BrickboardTheme.colours,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val replacementTypography = BrickboardTypography
+    val currentColor = remember { if (darkTheme) darkPalette() else colours }
+    val rememberedColors = remember { currentColor.copy() }.apply { updateColorsFrom(currentColor) }
+
     CompositionLocalProvider(
-        LocalReplacementTypography provides replacementTypography
+        LocalReplacementColours provides rememberedColors,
+        LocalReplacementTypography provides typography,
     ) {
-        MaterialTheme(
-            content = content,
-            colors = lightColourPalette
-        )
+        ProvideTextStyle(typography.body, content = content)
     }
 }
 
 object BrickboardTheme {
-    val typography: CustomTypography
+    val typography: BrickboardTypography
         @Composable
+        @ReadOnlyComposable
         get() = LocalReplacementTypography.current
+
+    val colours: BrickboardColours
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalReplacementColours.current
+
 }
