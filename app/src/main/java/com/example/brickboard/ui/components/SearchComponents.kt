fb4ad0e7ba672507add462caf6c783a372e3cc83
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2023-05-22 15:32:17 -0400

    Adapted the Theme grid item to use lego theme objects instead of a string. Other changes too

--- app/src/main/java/com/example/brickboard/ui/components/SearchComponents.kt
+++ app/src/main/java/com/example/brickboard/ui/components/SearchComponents.kt
@@ -1,6 +1,5 @@
 package com.example.brickboard.ui.components
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -17,55 +16,42 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.brickboard.R
+import com.example.brickboard.repository.LegoTheme
 import com.example.brickboard.ui.theme.BrickboardTheme
-import com.example.brickboard.utils.stringToHSLColor
+import com.example.brickboard.utils.stringToTint
 
 @Composable
 fun ThemeGridItem(
-    title: String,
-    @DrawableRes imageResource: Int,
-    onClick : () -> Unit = {}
+    theme: LegoTheme,
+    onClick: () -> Unit = {}
 ){
-    val colourLong = stringToHSLColor(title)
+    val colourLong = theme.tint ?: stringToTint(theme.name)
     Box(
         modifier = Modifier
-            .clickable(onClick = onClick)
             .size(160.dp, 110.dp)
             .clip(RoundedCornerShape(16.dp))
+            .clickable(onClick = onClick)
     ) {
         Image(
-            painter = painterResource(id = imageResource),
+            painter = painterResource(id = theme.image),
             contentDescription = null,
             contentScale = ContentScale.Crop,
         )
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(colourLong).copy(alpha = 0.7f))
+                .background(Color(colourLong.hex).copy(alpha = 0.7f))
 
         )
         Text(
-            text = title,
+            text = theme.name,
             color = Color.White,
             style = BrickboardTheme.typography.headline,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp, 16.dp)
 
-        )
-    }
-}
-
-@Preview
-@Composable
-fun ThemeGridItemPreview(){
-    BrickboardTheme() {
-        ThemeGridItem(
-            title = "Star Wars™",
-            imageResource = R.drawable.themeicon_starwars
         )
     }
 }
